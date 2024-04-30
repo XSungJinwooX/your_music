dc7ddc4d8fecd6edf057cbe3769b0278b2ed82d2
XSungJinwooX <xsungjinwoox0602@gmail.com>
2024-04-30 09:11:56 -0400

    Create the model: Range

--- apps/people/models.py
+++ apps/people/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Range(models.Model):
+    range_name = models.CharField(max_length=100)
+    multiplier = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(9)]
+    )
+
+    def __str__(self):
+        return f"{self.range_name} (Multiplier: {self.multiplier})"
--- your_music/settings.py
+++ your_music/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!f!=hrj=pck077bxj!9z1kfhr-srr=depcos+mumc+9w#*jp4$'
+SECRET_KEY = 'django-insecure-w#tb&rc*pzwv^)ihwx)l$+pjf%0axzq6xu3n5(xxz#mi@&nmlj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.people',
+    'apps.people'
 ]
 
 MIDDLEWARE = [
